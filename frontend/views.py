aee5af6934afc8644c2c88c83cbf511eb2f781f1
nick p <nicholas.pasqualetti@gmail.com>
2023-01-21 21:27:41 -0500

    stuff

--- frontend/views.py
+++ frontend/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.core.mail import send_mail
+from django.conf import settings
 # Create your views here.
 
 def index(request):
@@ -11,6 +13,17 @@ def about(request):
     return render(request, 'about.html', context)  
 
 def contact(request):
+    if request.method == 'POST':
+        print(dir(request.POST))
+        print(request.POST)
+        send_mail(
+            subject='Hi',
+            message='Test',
+            from_email=settings.EMAIL_HOST_USER,
+            recipient_list=['nick@technetconsultingllc.com']
+        )
+        messages.success(request, 'We have received your email. We will be in contact within 24 hours.')
+        return redirect('/')
     context = {}
     return render(request, 'contact.html', context)  
 
